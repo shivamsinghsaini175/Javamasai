faca7699106f05896d739652fd555a2106619ae1
shivamsinghsaini175 <122208475+shivamsinghsaini175@users.noreply.github.com>
2025-08-05 14:47:55 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
-public class Main {
 
-    public static void main(String[] args) {
-         Employee e1= new Employee();
+        public class Main {
+            public static void main(String[] args) {
+                Address a1 =new Address("jammu","j&k",180004 );
+                Person p1=new Person("Shivam",22,a1);
 
-        Employee e2= new Employee("shiva","bombay");
-         e1.setEmployeeName("shivam");
-         e1.setEmployeeDesignation("get");
-         e1.setEmployeeSalary(1000000);
-         e1.setEmployeeAddress("jammu");
-         e1.setEmployeeDob("14/12/2003");
+                Student s1=new Student(p1.getName(), p1.getAge(),a1,102,"8.2cgpa");
 
+                Emp e3=new Emp(p1.getName(), p1.getAge(),a1,279993,"Ass.Manager",62000);
 
+                System.out.println(s1);
+                System.out.println();
 
-        System.out.println(e1.toString());
-        System.out.println(e2.toString());
+                System.out.println(e3);
+                System.out.println();
+
 
     }
 }
